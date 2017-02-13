0efcea0215dc3e858283332f9897027b628a955e
Michael Wolbert <michael.wolbert@werockit.nl>
2017-02-13 16:22:07 +0100

    Remove plugins, should be added by user

--- src/tinymce-editor-settings.service.ts
+++ src/tinymce-editor-settings.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
 export class TinymceEditorSettingsService {
   skin_url = '/assets/tinymce/skins/lightgray';
   toolbar = 'undo redo | styleselect | bold italic | link image';
-  plugins = 'link paste';
   schema = 'html5';
 }
 
