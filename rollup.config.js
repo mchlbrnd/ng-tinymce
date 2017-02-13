af9619428719f3556e0bea2254f2c44de9969a78
Michael Wolbert <michael.wolbert@werockit.nl>
2017-02-13 16:22:34 +0100

    Remove paths from rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -8,12 +8,5 @@ export default {
     '@angular/core': 'ng.core',
     '@angular/common': 'ng.core',
     '@angular/forms': 'ng.forms'
-  },
-  paths: [
-    'node_modules/tinymce/tinymce.js',
-    'node_modules/tinymce/themes/modern/theme.js',
-    'node_modules/tinymce/themes/inlite/theme.js',
-    'node_modules/tinymce/plugins/link/plugin.js',
-    'node_modules/tinymce/plugins/paste/plugin.js'
-  ]
+  }
 }
